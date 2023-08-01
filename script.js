7d8d1ca703eec330ca06104bcd5c4a8250f8921b
Mohibkhan98 <mutayyabkhan98@gmail.com>
2023-08-01 19:44:15 +0530

    PortFolio_Updated

--- script.js
+++ script.js
@@ -65,12 +65,12 @@ var resumeLink = document.querySelector("#resume-button-1");
 resumeLink.addEventListener("click", function (event) {
   event.preventDefault();
   window.open(
-    "https://drive.google.com/file/d/1NIzIEpbnQrmZIUrpaYxwuB69VsguRb-4/view?usp=sharing",
+    "https://drive.google.com/file/d/1PkS2JxSdYhdIoGfYTG5atRWDvVUCOZoN/view?usp=sharing",
     "_blank"
   );
   let link = document.createElement("a");
   link.href =
-    "https://drive.google.com/uc?export=download&id=1NIzIEpbnQrmZIUrpaYxwuB69VsguRb-4";
+    "https://drive.google.com/uc?export=download&id=1PkS2JxSdYhdIoGfYTG5atRWDvVUCOZoN";
   link.download = "MUTAYYAB-MOHIB-RESUME";
   link.click();
 });
@@ -79,12 +79,12 @@ var resumeLink = document.querySelector("#resume-button-2");
 resumeLink.addEventListener("click", function (event) {
   event.preventDefault();
   window.open(
-    "https://drive.google.com/file/d/1NIzIEpbnQrmZIUrpaYxwuB69VsguRb-4/view?usp=sharing",
+    "https://drive.google.com/file/d/1PkS2JxSdYhdIoGfYTG5atRWDvVUCOZoN/view?usp=sharing",
     "_blank"
   );
   let link = document.createElement("a");
   link.href =
-    "https://drive.google.com/uc?export=download&id=1NIzIEpbnQrmZIUrpaYxwuB69VsguRb-4";
+    "https://drive.google.com/uc?export=download&id=1PkS2JxSdYhdIoGfYTG5atRWDvVUCOZoN";
   link.download = "MUTAYYAB-MOHIB-RESUME";
   link.click();
 });
